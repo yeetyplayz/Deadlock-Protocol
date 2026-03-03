78f60834c92ad328e877f943de2c7668dbd6e8c3
yeetyplayz <remcoklaassen077@gmail.com>
2026-03-03 10:22:49 +0100

    added gunBehaviour and sounds

---
+++ Deadlock Protocol/Assets/Scripts/Gun/gunBehaviour.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class gunBehaviour : MonoBehaviour
+{
+    [Header("Kind of Gun")]
+    public bool pistol;
+    public GameObject pistolShot;
+    public bool shotgun;
+    private int shotgunBullets = 8;
+    public bool machineGun;
+    public bool miniGun;
+    public bool rifle;
+    public bool sniper;
+    private int curAmmo;
+    private int curMaxAmmo;
+
+    private float laserRange;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (pistol)
+            {
+                UsePistol();
+            }
+            if (shotgun) 
+            {
+                while (shotgunBullets > 0)
+                {
+                    shotgunBullets--;
+                }
+            }
+            if (machineGun)
+            { 
+            
+            }
+            if (miniGun) 
+            {
+            
+            }
+            if (rifle)
+            {
+
+            }
+            if (sniper)
+            {
+
+            }
+            else 
+            {
+                Debug.Log("PROBLEM: NO GUN FOUND");
+            }
+        }
+    }
+    private void UsePistol()
+    {
+        
+    }
+}
