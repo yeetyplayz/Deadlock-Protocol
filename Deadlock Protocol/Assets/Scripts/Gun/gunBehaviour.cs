cc61237876be09ce67a1e77b61e7e5dfd48fb5d7
yeetyplayz <remcoklaassen077@gmail.com>
2026-03-03 13:25:30 +0100

    Gun sound

--- Deadlock Protocol/Assets/Scripts/Gun/gunBehaviour.cs
+++ Deadlock Protocol/Assets/Scripts/Gun/gunBehaviour.cs
@@ -4,7 +4,7 @@ public class gunBehaviour : MonoBehaviour
 {
     [Header("Kind of Gun")]
     public bool pistol;
-    public GameObject pistolShot;
+    public AudioSource pistolShot;
     public bool shotgun;
     private int shotgunBullets = 8;
     public bool machineGun;
@@ -19,7 +19,7 @@ public class gunBehaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -62,6 +62,6 @@ public class gunBehaviour : MonoBehaviour
     }
     private void UsePistol()
     {
-        
+        pistolShot.Play();
     }
 }
