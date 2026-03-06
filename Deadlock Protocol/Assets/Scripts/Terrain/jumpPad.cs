39608c13d9d28d6eafcec276e3f63096950afb5b
yeetyplayz <remcoklaassen077@gmail.com>
2026-03-06 01:38:39 +0100

    Jump pad
    Added functional jump pad script

---
+++ Deadlock Protocol/Assets/Scripts/Terrain/jumpPad.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class jumpPad : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int jumpForce = 15;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
