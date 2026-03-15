ebcb453b2712e4d95ba7c3624089d42f3c431d59
yeetyplayz <remcoklaassen077@gmail.com>
2026-03-15 01:39:46 +0100

    fixing guns
    tryna fix the gun scripts

--- Deadlock Protocol/Assets/Scripts/Player/Gun/gun.cs
+++ Deadlock Protocol/Assets/Scripts/Player/Gun/gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class gun : MonoBehaviour
 {
@@ -6,26 +7,43 @@ public class gun : MonoBehaviour
     public float damage;
     public float firerate;
     public float reloadSpeed;
+    public bool isReloading;
+    public bool pew;
     public int ammo;
     public int maxAmmo;
+    public string bug;
     public virtual void Fire()
     {
-        Debug.Log("BOOM");
+        if(pew == false)
+        {
+            StartCoroutine(FireRoutine());
+        }
     }
     public virtual void Reload()
     {
-        Debug.Log("Realoading...");
+        if(isReloading == false)
+        {
+            StartCoroutine(ReloadRoutine());
+        }
+    }
+    IEnumerator ReloadRoutine()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+        yield return new WaitForSeconds(reloadSpeed);
+        Debug.Log("Reload complete!");
+        ammo = maxAmmo;
+        isReloading = false;
     }
 
-    public void Update()
+    IEnumerator FireRoutine()
     {
-        if(ammo > 0 && Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Fire();
-        }
-        if(ammo == 0 && Input.GetKeyDown(KeyCode.R))
-        {
-            Reload();
-        }
+        pew = true;
+        --ammo;
+        Debug.Log(bug);
+        yield return new WaitForSeconds(firerate);
+        pew = false;
     }
+
+    
 }
--- Deadlock Protocol/Assets/Scripts/Player/Gun/sniper.cs
+++ Deadlock Protocol/Assets/Scripts/Player/Gun/sniper.cs
@@ -1,9 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class sniper : gun
 {
+    private void Start()
+    {
+        damage = 50f;
+        firerate = 2.5f;
+        reloadSpeed = 4.3f;
+        ammo = 5;
+        maxAmmo = 5;
+        bug = "sniper BOOM";
+    }
     public override void Fire()
     {
-        Debug.Log("sniper BOOM");
+        base.Fire();
+    }
+    public override void Reload()
+    {
+        base.Reload();
+    }
+    public void Update()
+    {
+        if (pew = false && ammo > 0 && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+        if (isReloading = false && ammo < maxAmmo && Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
     }
 }
